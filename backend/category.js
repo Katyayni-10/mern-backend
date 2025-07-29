6cab243635b5537f17391062f34659ffcd26c154
Katyayni-10 <katyaynibajaj@gmail.com>
2025-07-30 01:03:35 +0530

    category.js

--- backend/category.js
+++ backend/category.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const router = express.Router();
-const Category = require("./category1"); // adjust path if needed
+// const express = require("express");
+// const router = express.Router();
+// const Category = require("./category1"); // adjust path if needed
 
-// GET /api/categories
-router.get("/categories", async (req, res) => {
-  try {
-    const categories = await Category.find({});
-    res.status(200).json(categories);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+// // GET /api/categories
+// router.get("/categories", async (req, res) => {
+//   try {
+//     const categories = await Category.find({});
+//     res.status(200).json(categories);
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
