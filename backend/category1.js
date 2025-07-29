0694cc2cd11a760e43ac57372253dbe902e2c3a9
Katyayni-10 <katyaynibajaj@gmail.com>
2025-07-30 01:03:52 +0530

    category1.js

--- backend/category1.js
+++ backend/category1.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const category = new mongoose.Schema({
-  name: String,
-  image: String,
-  route: String,
-});
+// const category = new mongoose.Schema({
+//   name: String,
+//   image: String,
+//   route: String,
+// });
 
-module.exports = mongoose.model("category", CategorySchema);
+// module.exports = mongoose.model("category", CategorySchema);
